55b9f815a4bb5a09ea10304caaf4c3c5bbf07f64
github username <github email address>
2018-10-21 20:43:10 -0400

    able to unlike/unsave

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,18 +1,27 @@
 class Api::V1::RecipesController < ApplicationController
-  skip_before_action :authorized, only: [:index, :create, :show_recipes]
+  skip_before_action :authorized, only: [:index, :create, :show_recipes, :destroy]
 
   def index
     @recipes = Recipe.all
     render json: @recipes
   end
 
-  ####################### Not working. Might be because of strong params.
   def create
     @recipe = Recipe.create(recipe_object: params[:recipe][:recipe_object], user_id: recipe_params[:user_id])
     if @recipe.valid?
       render json: { recipe: RecipeSerializer.new(@recipe) }, status: :created
     else
-      render json: {errors: "Recipe did not save"}, status: :not_acceptable
+      render json: { errors: "Recipe did not save" }, status: :not_acceptable
+    end
+  end
+
+  def destroy
+    byebug
+    @recipe = Recipe.find_by(recipe_object: params[:recipe][:recipe_object], user_id: recipe_params[:user_id])
+    if @recipe.destroy
+      render json: { message: "Recipe unsaved" }, status: :accepted
+    else
+      render json: { errors: "Recipe unsave failed" }, status: :not_acceptable
     end
   end
 
@@ -20,9 +29,6 @@ class Api::V1::RecipesController < ApplicationController
     url = "http://api.yummly.com/v1/api/recipes?_app_id=" + ENV["SECRET_ONE"] + "&_app_key=" + ENV["SECRET_TWO"] + params["ingredients"] + "&maxResult=400&start=400"
     request = RestClient.get(url)
     response = JSON.parse(request)
-    # response.matches.each do |recipe|
-    #   Recipe.create(alkrlkjaerg: recipe.algerjlaejrg)
-    # end
     render json: response
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create, :current_user]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
-      resources :recipes, only: [:index, :create]
+      resources :recipes, only: [:index, :create, :destroy]
+      delete '/recipes', to: 'recipes#destroy'
       post '/show_recipes', to: "recipes#show_recipes"
     end
   end
